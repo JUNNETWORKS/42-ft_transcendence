7c6a5d02a8b1da503287d2f9ad6c69080b2f6e00
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-08-26 14:09:22 +0900

    add prisma module

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  async enableShutdownHook(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
