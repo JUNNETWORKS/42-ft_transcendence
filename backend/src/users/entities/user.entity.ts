1cda0cb0b47867fee04f5bddc32d9e4bcdd9dba2
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-08-26 15:28:30 +0900

    userEntity実装、APIレスポンスデコレーター追加

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id!: number;
+
+  @ApiProperty()
+  email!: string;
+
+  @ApiProperty()
+  display_name!: string;
+}
