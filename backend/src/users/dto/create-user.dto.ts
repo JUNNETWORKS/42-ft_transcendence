10f40f89209f514757e3f5fb7225649f7ae93962
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-08-26 15:13:38 +0900

    最小のCRUD実装

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+// https://github.com/nestjs/nest/issues/4178
+
+// swaggerのデコレーターは自動生成可
+// https://docs.nestjs.com/openapi/cli-plugin
+
+export class CreateUserDto {
+  @ApiProperty()
+  email!: string;
+
+  @ApiProperty()
+  display_name!: string;
+}
