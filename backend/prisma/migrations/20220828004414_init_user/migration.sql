9ca69f040cc697b0003a460448576511dbc99b2c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-08-28 09:45:02 +0900

    モデル修正

--- backend/prisma/migrations/20220828004414_init_user/migration.sql
+++ backend/prisma/migrations/20220828004414_init_user/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "display_name" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -11,4 +11,4 @@ CREATE TABLE "User" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_display_name_key" ON "User"("display_name");
+CREATE UNIQUE INDEX "User_displayName_key" ON "User"("displayName");
