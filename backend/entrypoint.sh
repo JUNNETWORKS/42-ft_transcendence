f36bdac31e9b131fb2a2d3ef08d796012d2f213a
Junichi Sasaki <junichi200123@gmail.com>
2022-08-30 02:12:15 +0900

    node_modules/ の権限エラー修正

---
+++ backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# node_modules/ の権限エラー回避
+# https://qiita.com/yohm/items/047b2e68d008ebb0f001
+
+set -ux
+
+USER_ID=${LOCAL_UID:-9001}
+GROUP_ID=${LOCAL_GID:-9001}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID, pwd: $(pwd)"
+useradd -u $USER_ID -o -m user
+groupmod -g $GROUP_ID user
+export HOME=/home/user
+
+chown -R user:user node_modules/
+
+exec /usr/sbin/gosu user "$@"
