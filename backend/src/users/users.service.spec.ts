ba2896931264ee2d44b821517435e5e0dffa971c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-09-02 17:00:30 +0900

    ユニークな値の重複テスト

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -29,4 +29,26 @@ describe('UsersService', () => {
     expect(user.email).toBe(email);
     expect(user.displayName).toBe(displayName);
   });
+
+  it('create user twice with same email', async () => {
+    const email = 'hogehoge@gmail.com';
+    const displayName = 'hoge123';
+    const user = await service.create({ email, displayName });
+    expect(user.email).toBe(email);
+    expect(user.displayName).toBe(displayName);
+    await expect(
+      service.create({ email, displayName: 'fuga123' })
+    ).rejects.toThrow();
+  });
+
+  it('create user twice with same displayName', async () => {
+    const email = 'hogehoge@gmail.com';
+    const displayName = 'hoge123';
+    const user = await service.create({ email, displayName });
+    expect(user.email).toBe(email);
+    expect(user.displayName).toBe(displayName);
+    await expect(
+      service.create({ email: 'fugafuga@gmail.com', displayName })
+    ).rejects.toThrow();
+  });
 });
