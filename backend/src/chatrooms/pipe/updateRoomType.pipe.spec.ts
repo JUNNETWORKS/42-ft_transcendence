b7275c15a16cc0d14d588252d69f6e7e057e2ae7
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-09-04 13:30:25 +0900

    add updateRoomType.pipe

---
+++ backend/src/chatrooms/pipe/updateRoomType.pipe.spec.ts
@@ -0,0 +1,47 @@
+import { ArgumentMetadata, HttpException } from '@nestjs/common';
+import { UpdateRoomTypeDto } from '../dto/updateRoomType.dto';
+import { UpdateRoomTypePipe } from './updateRoomType.pipe';
+
+describe('UpdateRoomTypePipe', () => {
+  it('should be defined', () => {
+    expect(new UpdateRoomTypePipe()).toBeDefined();
+  });
+
+  it('roomPasswordが必要のないタイプのルームのプロパティにある。', async () => {
+    const target: UpdateRoomTypePipe = new UpdateRoomTypePipe();
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: UpdateRoomTypeDto,
+      data: '',
+    };
+    const dto: UpdateRoomTypeDto = {
+      roomType: 'PUBLIC',
+      roomPassword: 'testpassword',
+    };
+    expect(() => {
+      target.transform(dto, metadata);
+    }).toThrow(
+      new HttpException(
+        'roomPassword is needed when roomType is only "LOCKED"',
+        400
+      )
+    );
+  });
+
+  it('roomType LOCKED にroomPasswordがない', async () => {
+    const target: UpdateRoomTypePipe = new UpdateRoomTypePipe();
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: UpdateRoomTypeDto,
+      data: '',
+    };
+    const dto: UpdateRoomTypeDto = {
+      roomType: 'LOCKED',
+    };
+    expect(() => {
+      target.transform(dto, metadata);
+    }).toThrow(
+      new HttpException('roomPassword is needed with "LOCKED" roomType', 400)
+    );
+  });
+});
