bce106dd626eb5c889477f43e8b34e337de88533
Taisei Toya <tytaisei0102@gmail.com>
2022-09-26 21:41:19 +0200

    カラーを設定

--- frontend/src/providers/AppProvider.tsx
+++ frontend/src/providers/AppProvider.tsx
@@ -1,9 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { Theme } from '@/providers/Theme';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>{children}</BrowserRouter>
+    </ThemeProvider>
+  );
 };
---
+++ frontend/src/providers/Theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#353535',
+    },
+    secondary: {
+      main: '#5E5E5E',
+    },
+  },
+});
