21906ab09fde93a6437724868731fd43adf98bf7
Taisei Toya <tytaisei0102@gmail.com>
2022-09-29 14:03:30 +0200

    Layoutコンポーネントを作成
    NavBarをLayoutに移動

--- frontend/src/features/Index/Index.tsx
+++ frontend/src/features/Index/Index.tsx
@@ -1,5 +1,3 @@
-import { NavBar } from '@/Components/NavBar';
-
 export const Index = () => {
-  return <NavBar></NavBar>;
+  return <div>Hello Index Page.</div>;
 };
--- frontend/src/providers/AppProvider.tsx
+++ frontend/src/providers/AppProvider.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { Theme } from '@/providers/Theme';
+import { Layout } from '@/providers/Layout';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -9,7 +10,9 @@ type AppProviderProps = {
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <ThemeProvider theme={Theme}>
-      <BrowserRouter>{children}</BrowserRouter>
+      <BrowserRouter>
+        <Layout>{children}</Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
