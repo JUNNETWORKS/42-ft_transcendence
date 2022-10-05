99373fdd2fc3d01425afff7caab81c3a6b8d2559
河田鷹介 <benefium@gmail.com>
2022-10-05 12:53:32 +0900

    チャットAPI (2)

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -13,7 +13,10 @@ import { OperationJoinDto } from 'src/chatrooms/dto/operation-join.dto';
 import { OperationLeaveDto } from 'src/chatrooms/dto/operation-leave.dto';
 import { OperationOpenDto } from 'src/chatrooms/dto/operation-open.dto';
 import { OperationSayDto } from 'src/chatrooms/dto/operation-say.dto';
+import { UsersService } from 'src/users/users.service';
 import { ChatService } from './chat.service';
+
+let iRoomId = 10;
 
 @WebSocketGateway({
   cors: true,
@@ -25,20 +28,72 @@ export class ChatGateway implements OnGatewayConnection {
 
   constructor(
     private readonly chatService: ChatService,
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService
   ) {}
 
   /**
    * 新しいクライアントが接続してきた時の処理
    * @param client
    */
-  handleConnection(@ConnectedSocket() client: Socket) {
-    const payload = this.trapAuth(client);
-    const sub = payload['sub'];
-    if (sub) {
-      this.joinChannel(client, 'User', sub);
-      this.joinChannel(client, 'Global', 'global');
+  async handleConnection(@ConnectedSocket() client: Socket) {
+    const user = await this.trapAuth(client);
+    if (!user) {
+      return;
     }
+    const userId = user.id;
+    // システムチャンネルへのjoin
+    this.joinChannel(client, 'User', userId);
+    this.joinChannel(client, 'Global', 'global');
+    // TODO: ユーザがjoinしているチャットルーム(ハードリレーション)の取得
+    // TODO: roomへのjoin状態をハードリレーションに同期させる
+    // (connectionでは入室それ自体の通知は不要)
+    // オンライン状態の変化を通知
+    this.sendResults(
+      'ft_connection',
+      {
+        userId,
+        displayName: user.displayName,
+        connectedAt: new Date(),
+      },
+      {
+        global: 'global',
+      }
+    );
+  }
+
+  /**
+   * チャットルームを作成する
+   * @param data
+   * @param client
+   */
+  @SubscribeMessage('ft_open')
+  async handleOpen(
+    @MessageBody() data: OperationOpenDto,
+    @ConnectedSocket() client: Socket
+  ) {
+    const user = await this.trapAuth(client);
+    if (!user) {
+      {
+        return;
+      }
+    }
+    data.callerId = user.id;
+    // TODO: パラメータが正しければチャットルームを作成する
+    const roomId = iRoomId;
+    iRoomId += 1;
+    // 作成されたチャットルームにjoin
+    await this.usersJoin(user.id, 'ChatRoom', roomId);
+    // 新しいチャットルームが作成されたことを通知する
+    this.sendResults(
+      'ft_open',
+      {
+        roomId,
+      },
+      {
+        global: 'global',
+      }
+    );
   }
 
   /**
@@ -46,23 +101,31 @@ export class ChatGateway implements OnGatewayConnection {
    * @param data
    * @param client
    */
-  @SubscribeMessage('say')
+  @SubscribeMessage('ft_say')
   async handleSay(
     @MessageBody() data: OperationSayDto,
     @ConnectedSocket() client: Socket
   ) {
-    const { sub: callerId } = this.trapAuth(client);
-    data.callerId = callerId;
+    const user = await this.trapAuth(client);
+    if (!user) {
+      {
+        return;
+      }
+    }
+    data.callerId = user.id;
+    // TODO: 対象チャットルームの存在確認
+    // TODO: 実行者がチャットルームで発言可能であることの確認
     const roomId = data.roomId;
+    // 発言を作成
     const chatMessage = await this.chatService.postMessageBySay(data);
-    this.downlink(
-      'say',
+    // 発言内容を通知
+    this.sendResults(
+      'ft_say',
       {
         ...chatMessage,
-        displayName: await this.chatService.getDisplayName(chatMessage.userId),
+        displayName: user.displayName,
       },
       {
-        userId: callerId,
         roomId,
       }
     );
@@ -73,26 +136,32 @@ export class ChatGateway implements OnGatewayConnection {
    * @param data
    * @param client
    */
-  @SubscribeMessage('join')
+  @SubscribeMessage('ft_join')
   async handleJoin(
     @MessageBody() data: OperationJoinDto,
     @ConnectedSocket() client: Socket
   ) {
-    const { sub: callerId } = this.trapAuth(client);
-    data.callerId = callerId;
+    const user = await this.trapAuth(client);
+    if (!user) {
+      {
+        return;
+      }
+    }
+    data.callerId = user.id;
     const roomId = data.roomId;
-    // TODO: ハードリレーション
-    this.server
-      .in(this.fullRoomName('User', callerId))
-      .socketsJoin(this.fullRoomName('ChatRoom', roomId));
-    this.downlink(
-      'join',
+    // TODO: 入室対象のチャットルームが存在していることを確認
+    // TODO: 実行者が対象チャットルームに入室できることを確認
+    // TODO: ハードリレーション更新
+    // roomへのjoin状態をハードリレーションに同期させる
+    await this.usersJoin(user.id, 'ChatRoom', roomId);
+    // 入室したことを通知
+    this.sendResults(
+      'ft_join',
       {
         ...data,
-        displayName: await this.chatService.getDisplayName(callerId),
+        displayName: user.displayName,
       },
       {
-        userId: callerId,
         roomId,
       }
     );
@@ -103,32 +172,37 @@ export class ChatGateway implements OnGatewayConnection {
    * @param data
    * @param client
    */
-  @SubscribeMessage('leave')
+  @SubscribeMessage('ft_leave')
   async handleLeave(
     @MessageBody() data: OperationLeaveDto,
     @ConnectedSocket() client: Socket
   ) {
-    const { sub: callerId } = this.trapAuth(client);
-    data.callerId = callerId;
+    const user = await this.trapAuth(client);
+    if (!user) {
+      {
+        return;
+      }
+    }
+    data.callerId = user.id;
     const roomId = data.roomId;
-    // TODO: ハードリレーション
-    this.server
-      .in(this.fullRoomName('User', callerId))
-      .socketsLeave(this.fullRoomName('ChatRoom', roomId));
-    this.downlink(
-      'leave',
+    // TODO: 退出対象のチャットルームが存在していることを確認
+    // TODO: 実行者が対象チャットルームに入室していることを確認
+    // TODO: ハードリレーション更新
+    // roomへのjoin状態をハードリレーションに同期させる
+    await this.usersLeave(user.id, 'ChatRoom', roomId);
+    this.sendResults(
+      'ft_leave',
       {
         ...data,
-        displayName: await this.chatService.getDisplayName(callerId),
+        displayName: user.displayName,
       },
       {
-        userId: callerId,
         roomId,
       }
     );
   }
 
-  private trapAuth(client: Socket) {
+  private async trapAuth(client: Socket) {
     if (client.handshake.auth) {
       const token = client.handshake.auth.token;
       if (token) {
@@ -138,11 +212,18 @@ export class ChatGateway implements OnGatewayConnection {
         // console.log(verified);
         const decoded = this.jwtService.decode(token);
         if (decoded && typeof decoded === 'object') {
-          return decoded;
+          const sub = decoded['sub'];
+          if (sub) {
+            const userId = parseInt(sub);
+            const user = await this.usersService.findOne(userId);
+            if (user) {
+              return user;
+            }
+          }
         }
       }
     }
-    return {};
+    return null;
   }
 
   /**
@@ -163,6 +244,49 @@ export class ChatGateway implements OnGatewayConnection {
     return `${roomSuffix}${roomName}`;
   }
 
+  /**
+   * 指定したユーザIDに対応するクライアントを指定したルームにjoinさせる\
+   * **あらかじめユーザルーム(${userId})にjoinしているクライアントにしか効果がないことに注意！！**
+   * @param userId
+   * @param roomType
+   * @param roomName
+   */
+  private async usersJoin(
+    userId: number,
+    roomType: 'ChatRoom' | 'User' | 'Global',
+    roomName: any
+  ) {
+    const fullUserRoomName = this.fullRoomName('User', userId);
+    const fullChatRoomName = this.fullRoomName(roomType, roomName);
+    const socks = await this.server.in(fullUserRoomName).allSockets();
+    console.log(
+      `joining clients in ${fullUserRoomName} -> ${fullChatRoomName}`,
+      socks
+    );
+    this.server.in(fullUserRoomName).socketsJoin(fullChatRoomName);
+  }
+
+  /**
+   * (英語としておかしいので名前を変えること)
+   * @param userId
+   * @param roomType
+   * @param roomName
+   */
+  private async usersLeave(
+    userId: number,
+    roomType: 'ChatRoom' | 'User' | 'Global',
+    roomName: any
+  ) {
+    const fullUserRoomName = this.fullRoomName('User', userId);
+    const fullChatRoomName = this.fullRoomName(roomType, roomName);
+    const socks = await this.server.in(fullUserRoomName).allSockets();
+    console.log(
+      `leaveing clients in ${fullUserRoomName} from ${fullChatRoomName}`,
+      socks
+    );
+    this.server.in(fullUserRoomName).socketsLeave(fullChatRoomName);
+  }
+
   private joinChannel(
     @ConnectedSocket() client: Socket,
     roomType: 'ChatRoom' | 'User' | 'Global',
@@ -173,7 +297,7 @@ export class ChatGateway implements OnGatewayConnection {
     console.log(`client ${client.id} joined to ${fullRoomName}`);
   }
 
-  private downlink(
+  private async sendResults(
     op: string,
     payload: any,
     target: {
@@ -183,13 +307,13 @@ export class ChatGateway implements OnGatewayConnection {
     }
   ) {
     if (typeof target.userId === 'number') {
-      this.sendDownlink(op, 'User', target.userId, payload);
+      await this.sendResultRoom(op, 'User', target.userId, payload);
     }
     if (typeof target.roomId === 'number') {
-      this.sendDownlink(op, 'ChatRoom', target.roomId, payload);
+      await this.sendResultRoom(op, 'ChatRoom', target.roomId, payload);
     }
     if (target.global) {
-      this.sendDownlink(op, 'Global', target.global, payload);
+      await this.sendResultRoom(op, 'Global', target.global, payload);
     }
   }
 
@@ -199,14 +323,15 @@ export class ChatGateway implements OnGatewayConnection {
    * @param roomName
    * @param payload
    */
-  private sendDownlink(
+  private async sendResultRoom(
     op: string,
     roomType: 'ChatRoom' | 'User' | 'Global',
     roomName: any,
     payload: any
   ) {
     const fullName = this.fullRoomName(roomType, roomName);
-    console.log('sending downlink to:', fullName, payload);
+    const socks = await this.server.to(fullName).allSockets();
+    console.log('sending downlink to:', fullName, op, payload, socks);
     this.server.to(fullName).emit(op, payload);
   }
 }
--- frontend/src/features/Chat/Chat.tsx
+++ frontend/src/features/Chat/Chat.tsx
@@ -6,72 +6,124 @@ const socket = io('http://localhost:3000/chat', {
   auth: (cb) => {
     cb({
       token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inlva2F3YWRhQHN0dWRlbnQuNDJ0b2t5by5qcCIsInN1YiI6NSwiaWF0IjoxNjY0ODU5OTM2LjQ1NiwiZXhwIjoxNjY3NDUxOTM2LCJhdWQiOiJ0cmExMDAwIiwiaXNzIjoidHJhMTAwMCJ9.vudAYGwe5qdR9lBcK7zIlqpAU9_tRoeQvWhoeEjLmwc',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inlva2F3YWRhQHN0dWRlbnQuNDJ0b2t5by5qcCIsInN1YiI6NSwiaWF0IjoxNjY0OTAzODU3LjUwNSwiZXhwIjoxNjY3NDk1ODU3LCJhdWQiOiJ0cmExMDAwIiwiaXNzIjoidHJhMTAwMCJ9.z9hmpKZDEyBgxfTN70EyPPWJLsyUCrJKHUZkIBup4Do',
     });
   },
 });
 
-type AuxUserData = {
-  callerId: number;
-};
-
-type ArgmentSay = {
-  op: 'say';
-  roomId: number;
-  content: string;
-} & AuxUserData;
-
-function sendSay(
-  socket: Socket<DefaultEventsMap, DefaultEventsMap>,
-  payload: Omit<ArgmentSay, 'op'>
-) {
-  socket.emit('say', { op: 'say', ...payload });
-}
-
 export const Chat = () => {
+  type ChannelMessage = any;
+  const [roomList, setRoomList] = useState<string[]>([]);
+  // 今フォーカスしているチャットルームの表示用メッセージリスト
   const [messageList, setMessageList] = useState<string[]>([]);
-  const [message, setMessage] = useState('');
+  // 今発言用に入力しているメッセージ
+  const [content, setContent] = useState('');
+  // 今フォーカスしているチャットルームのID
+  const [currentRoomId, setCurrentRoomId] = useState<number>(1);
+  const [roomIdToJoin, setRoomIdToJoin] = useState('');
+
+  const messagesInChannel: { [key: string]: ChannelMessage[] } = {};
+  messagesInChannel[`#1`] = [];
 
   useEffect(() => {
-    socket.on('broadcast', (msg: any) => {
-      console.log('catch broadcast');
-      console.log(msg);
-      setMessageList((messageList) => [...messageList, msg.content]);
+    socket.on('ft_connection', (data: any) => {
+      console.log('catch connection');
+      console.log(data);
     });
 
-    socket.on('say', (msg: any) => {
+    socket.on('ft_say', (data: any) => {
       console.log('catch say');
-      console.log(msg);
-      setMessageList((messageList) => [...messageList, msg.content]);
+      console.log(data);
+      setMessageList((messageList) => [...messageList, data]);
+    });
+
+    socket.on('ft_join', (data: any) => {
+      console.log('catch join');
+      console.log(data);
     });
 
     return () => {
-      socket.off('broadcast');
-      socket.off('say');
+      socket.off('ft_connection');
+      socket.off('ft_say');
+      socket.off('ft_join');
     };
   }, []);
 
-  const sendMessage = () => {
+  const sendJoin = () => {
     const data = {
       roomId: 1,
       callerId: 1,
-      content: message,
     };
-    sendSay(socket, data);
+    console.log(data);
+    socket.emit('ft_join', data);
+  };
+
+  const sendSay = () => {
+    if (!currentRoomId) {
+      return;
+    }
+    const data = {
+      roomId: currentRoomId,
+      callerId: 1,
+      content: content,
+    };
+    console.log(data);
+    socket.emit('ft_say', data);
   };
 
   return (
     <div>
-      {messageList.map((data, index) => {
-        return <p key={index}>{data}</p>;
-      })}
-      <input
-        id="input"
-        autoComplete="off"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
+      <div className="message-list">
+        {messageList.map((data: any, index) => {
+          return (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+              key={index}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                }}
+              >
+                <div style={{ paddingRight: '4px' }}>
+                  displayName: {data.displayName}
+                </div>
+                <div style={{ paddingRight: '4px' }}>
+                  chatRoomId: {data.chatRoomId}
+                </div>
+                <div style={{ paddingRight: '4px' }}>
+                  createdAt: {data.createdAt}
+                </div>
+              </div>
+              <div>{data.content}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="input-panel">
+        <div className="content">
+          <input
+            id="input"
+            autoComplete="off"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button onClick={sendSay}>Send</button>
+        </div>
+        <div className="room">
+          <input
+            id="input"
+            autoComplete="off"
+            value={roomIdToJoin}
+            onChange={(e) => setRoomIdToJoin(e.target.value)}
+          />
+          <button onClick={sendJoin}>Join</button>
+        </div>
+      </div>
     </div>
   );
 };
