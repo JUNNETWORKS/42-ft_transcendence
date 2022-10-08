07995c15da08fffe42d71175d3ceabc6fa9dfa84
Taisei Toya <tytaisei0102@gmail.com>
2022-10-08 19:51:30 +0200

    Hogeをtailwindで編集

--- frontend/src/features/Hoge/Hoge.tsx
+++ frontend/src/features/Hoge/Hoge.tsx
@@ -1,5 +1,9 @@
-import { HogeText } from '@/features/Hoge/Components/HogeText';
-
 export const Hoge = () => {
-  return <HogeText text="Props Text!"></HogeText>;
+  return (
+    <div>
+      <h1 className=" text-3xl font-bold underline ">Hello world!</h1>
+      <p className=" text-red-500">aaaa</p>
+      <p>aaaa</p>
+    </div>
+  );
 };
