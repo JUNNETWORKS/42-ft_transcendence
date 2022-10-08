f789aee7e780b369da6135d96a5dc4d372ddaca0
Taisei Toya <tytaisei0102@gmail.com>
2022-10-08 15:20:35 +0200

    Revert "Merge pull request #16 from JUNNETWORKS/frontend_index_page"
    This reverts commit 746778a80bfc5b74d495265874cd040a7fa496f9, reversing
    changes made to d865f5b991acff031908bf22d3957539a7bdb195.

--- frontend/src/types/emotion.d.ts
+++
@@ -1,7 +0,0 @@
-import { Theme as MUTheme } from '@mui/material/styles';
-declare module '@emotion/react' {
-  //quick fixで提示される解決策ではemotion側からMUITheme内の値を呼び出した時
-  //予測変換されなかったため一行だけ無効にした
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends MUTheme {}
-}
