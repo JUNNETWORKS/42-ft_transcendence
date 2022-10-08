90e122da9bcb7e4786cbf376ef7825bc20f18d47
Taisei Toya <tytaisei0102@gmail.com>
2022-10-08 18:57:01 +0200

    fontの設定を追加

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["PixelMplus", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
