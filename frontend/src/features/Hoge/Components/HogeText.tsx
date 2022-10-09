9c67af1108f7bdbd8262b91c55aba1759babdc17
Taisei Toya <tytaisei0102@gmail.com>
2022-10-09 20:05:38 +0200

    Hogeが不要になったので削除

--- frontend/src/features/Hoge/Components/HogeText.tsx
+++
@@ -1,7 +0,0 @@
-type HogeTextProps = {
-  text: string;
-};
-
-export const HogeText = ({ text }: HogeTextProps) => {
-  return <div>HogeText is {text}</div>;
-};
--- frontend/src/features/Hoge/Hoge.tsx
+++
@@ -1,9 +0,0 @@
-export const Hoge = () => {
-  return (
-    <div>
-      <h1 className=" text-3xl font-bold underline ">Hello world!</h1>
-      <p className=" text-red-500">aaaa</p>
-      <p>aaaa</p>
-    </div>
-  );
-};
