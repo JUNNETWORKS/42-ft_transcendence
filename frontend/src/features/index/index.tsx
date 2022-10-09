80df332d4c75aeabbbea2f621b1256b67a52fd22
Taisei Toya <tytaisei0102@gmail.com>
2022-10-09 13:02:40 +0200

    indexpageを追加
    背景色を設定

---
+++ frontend/src/features/index/index.tsx
@@ -0,0 +1,12 @@
+export const Index = () => {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center gap-32 ">
+      <h1 className="text-8xl font-bold">ft_pong</h1>
+      <div className="flex flex-col gap-6">
+        <p className="text-5xl font-bold">Start</p>
+        <p className="text-5xl font-bold">Profile</p>
+        <p className="text-5xl font-bold">Social</p>
+      </div>
+    </div>
+  );
+};
