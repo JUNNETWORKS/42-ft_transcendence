a94ce1ce7e2e7688b586091574ddf7ba4d0ec14a
河田鷹介 <benefium@gmail.com>
2022-10-20 10:39:52 +0900

    ソケットからの受信処理をRouter直下の(表示実体のない)SocketHolderコンポーネントに移動 & 認証状態チェック処理もAuthCheckerコンポーネントに移動

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,56 +3,14 @@ import { Chat } from '@/features/Chat/Chat';
 import { Pong } from '@/features/Pong/components/Pong';
 import { Index } from '@/features/Index/Index';
 import { DevAuth } from '@/features/DevAuth/DevAuth';
-import {
-  authFlowStateAtom,
-  chatSocketAtom,
-  storedCredentialAtom,
-  userAtoms,
-} from '@/atoms';
+import { chatSocketAtom } from '@/atoms';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
-import { verifyCredential } from '@/auth';
 import { UserView } from '@/features/User/User';
+import { SocketHolder } from './SocketHolder';
+import { AuthChecker } from './AuthChecker';
 
 export const AppRoutes = () => {
-  // 「ソケット」
-  // 認証されていない場合はnull
   const [mySocket] = useAtom(chatSocketAtom);
-
-  // 認証フローのチェックと状態遷移
-  {
-    const [storedCredential] = useAtom(storedCredentialAtom);
-    const [authState, setAuthState] = useAtom(authFlowStateAtom);
-    const setPersonalData = useAtom(userAtoms.personalDataAtom)[1];
-
-    useEffect(() => {
-      switch (authState) {
-        case 'Neutral': {
-          setAuthState('Validating');
-          break;
-        }
-        case 'Validating':
-          // GET /auth/session を呼んでローカルのクレデンシャルをユーザ情報に変換
-          verifyCredential(
-            storedCredential,
-            (user) => {
-              // ユーザ情報に変換できた場合の処理
-              setPersonalData(user);
-              setAuthState('Authenticated');
-            },
-            () => {
-              // 変換できなかった場合の処理
-              setPersonalData(null);
-              setAuthState('NotAuthenticated');
-            }
-          );
-          break;
-        case 'NotAuthenticated':
-          break;
-      }
-    }, [authState]);
-  }
-
   const authElement = <DevAuth />;
   const guardElement = !mySocket ? authElement : null;
   const commonRoutes = [
@@ -64,5 +22,11 @@ export const AppRoutes = () => {
   ];
   const routeElements = useRoutes([...commonRoutes]);
 
-  return <>{routeElements}</>;
+  return (
+    <>
+      <AuthChecker />
+      <SocketHolder />
+      {routeElements}
+    </>
+  );
 };
