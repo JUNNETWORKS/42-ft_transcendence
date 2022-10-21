5171201a0c69098afa9fca99d04f33957c6aafec
Taisei Toya <tytaisei0102@gmail.com>
2022-10-21 18:40:19 +0200

    trapAuthをauthServiceの機能に移動
    chatの中で使いたいので、authServiceを受け取るよう変更

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { ChatGateway } from './chat.gateway';
 import { ChatroomsModule } from '../chatrooms/chatrooms.module';
 import { UsersModule } from '../users/users.module';
 import { JwtService } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [ChatGateway, ChatService, JwtService],
-  imports: [ChatroomsModule, UsersModule],
+  imports: [ChatroomsModule, UsersModule, AuthModule],
 })
 export class ChatModule {}
