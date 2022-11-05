d4d738308679a214d7d2a10a5fbb91a39800a74f
Junichi Sasaki <junichi200123@gmail.com>
2022-11-05 22:00:58 +0900

    OngoingMatchesクラスを作成

---
+++ backend/src/pong/game/ongoing-matches.ts
@@ -0,0 +1,46 @@
+import { Server } from 'socket.io';
+import { OnlineMatch } from './online-match';
+import { PlayerInput } from './types/game-state';
+
+// 全ての進行中のマッチ管理するクラス
+export class OngoingMatches {
+  private readonly wsServer: Server;
+  private matches: Map<string, OnlineMatch>;
+
+  constructor(wsServer: Server) {
+    this.wsServer = wsServer;
+  }
+
+  createMatch(userID1: number, userID2: number) {
+    const match = new OnlineMatch(this.wsServer, userID1, userID2);
+    if (match.getMatchID() in this.matches === false) {
+      this.matches.set(match.getMatchID(), match);
+    }
+  }
+
+  moveBar(playerID: number, playerAction: PlayerInput) {
+    const match = this.findMatchByPlayer(playerID);
+    if (match) {
+      match.moveBar(playerID, playerAction);
+    }
+  }
+
+  // プレイヤーが退出
+  leave(playerID: number) {
+    for (const matchID in this.matches) {
+      const match = this.matches.get(matchID);
+      if (match?.getPlayerIDs().includes(playerID)) {
+        match.leave(playerID);
+      }
+    }
+  }
+
+  findMatchByPlayer(playerID: number) {
+    for (const matchID in this.matches) {
+      const match = this.matches.get(matchID);
+      if (match?.getPlayerIDs().includes(playerID)) {
+        return match;
+      }
+    }
+  }
+}
