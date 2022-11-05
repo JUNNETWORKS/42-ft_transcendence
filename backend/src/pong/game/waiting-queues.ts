98390aa02cffb4502488c3a3ab806a138b7b73e4
Junichi Sasaki <junichi200123@gmail.com>
2022-11-05 21:43:18 +0900

    WaitingQueuesクラスを作成

---
+++ backend/src/pong/game/waiting-queues.ts
@@ -0,0 +1,44 @@
+import { OngoingMatches } from './ongoing-matches';
+import { WaitingQueue } from './waiting-queue';
+
+// 複数のマッチメイキングの待機キューを保持する｡
+// マッチメイキングの待機キューは以下のような種類がある｡
+// - Rank全体で1つ
+// - Casual全体で1つ
+// - Privateの場合は1試合につき1つ
+export class WaitingQueues {
+  private ongoingMatches: OngoingMatches;
+  private queues: Map<string, WaitingQueue>;
+
+  constructor(ongoingMatches: OngoingMatches) {
+    this.ongoingMatches = ongoingMatches;
+  }
+
+  // 待機キューを取得する
+  getQueue(id: string): WaitingQueue | undefined {
+    if (this.queues.has(id)) {
+      return this.queues.get(id);
+    }
+  }
+
+  // 待機キューを追加する
+  appendQueue(queue: WaitingQueue): void {
+    this.queues.set(queue.id, queue);
+  }
+
+  // 待機キューを削除する
+  removeQueue(id: string): void {
+    if (this.queues.has(id)) {
+      this.queues.delete(id);
+    }
+  }
+
+  // プレイヤーが含まれている待機キューを1つ返す
+  getQueueByPlayerID(userID: number): WaitingQueue | undefined {
+    for (const queue of this.queues.values()) {
+      if (queue.isUserExists(userID)) {
+        return queue;
+      }
+    }
+  }
+}
