2c22a611c4b312b68019240804b4f6037393a896
Junichi Sasaki <junichi200123@gmail.com>
2022-11-05 22:09:57 +0900

    pong.gateway.ts に待機キューの処理を追加

--- backend/src/pong/pong.gateway.ts
+++ backend/src/pong/pong.gateway.ts
@@ -7,47 +7,92 @@ import {
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 import { PongMatchActionDTO } from './dto/pong-match-action';
-import { OnlineMatch } from './game/online-match';
-
-// ========== WS: pong.match.action ==========
-
-const match: OnlineMatch = new OnlineMatch();
+import { OngoingMatches } from './game/ongoing-matches';
+import { WaitingQueues } from './game/waiting-queues';
+import { PongMatchMakingEntryDTO } from './dto/pong-match-making-entry';
+import { WaitingQueue } from './game/waiting-queue';
+import { PongMatchMakingCreateDTO } from './dto/pong-match-making-create';
+import { generateMatchID } from './game/utils';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({ cors: true, namespace: '/pong' })
 export class PongGateway {
   private wsServer!: Server;
+  private ongoingMatches: OngoingMatches;
+  private waitingQueues: WaitingQueues;
   private readonly logger = new Logger('Match WS');
+
+  constructor(private readonly authService: AuthService) {}
 
   afterInit(server: Server) {
     this.wsServer = server;
-    // 本来はOnlineMatchのコンストラクタで渡す
-    match.wsServer = server;
+    this.ongoingMatches = new OngoingMatches(this.wsServer);
+    this.waitingQueues = new WaitingQueues(this.ongoingMatches);
   }
 
   onApplicationBootstrap() {
+    // CASUAL の待機キューを作成
+    const rankQueue = new WaitingQueue('RANK', this.ongoingMatches);
+    const casualQueue = new WaitingQueue('CASUAL', this.ongoingMatches);
+    this.waitingQueues.appendQueue(rankQueue);
+    this.waitingQueues.appendQueue(casualQueue);
     return;
   }
 
-  onApplicationShutdown() {
-    match.close();
+  async handleDisconnect(client: Socket) {
+    const user = await this.authService.trapAuth(client);
+    if (!user) {
+      return;
+    }
+    this.ongoingMatches.leave(user.id);
+    const queue = this.waitingQueues.getQueueByPlayerID(user.id);
+    if (queue) {
+      queue.remove(user.id);
+    }
   }
 
-  handleConnection(client: Socket) {
-    this.logger.log(`WebSocket connection ID(${client.id}).`);
-    match.joinAsSpectator(client);
-    match.joinAsPlayer(client);
+  @SubscribeMessage('pong.match_making.create')
+  receiveMatchMakingCreate(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: PongMatchMakingCreateDTO
+  ) {
+    if (data.queueType === 'private') {
+      // プライベートマッチ用の待機キューを作成する｡
+      const matchID = generateMatchID();
+      const queue = new WaitingQueue(matchID, this.ongoingMatches, {
+        maxWaiters: 2,
+      }); // TODO: タイムアウトと最大人数を設定する
+      this.waitingQueues.appendQueue(queue);
+      // クライアントに待機キュー作成完了通知を送信
+      client.emit('pong.match_making.created', {
+        id: matchID,
+      });
+    }
   }
 
-  handleDisconnect(client: Socket) {
-    this.logger.log(`WebSocket disconnection ID(${client.id}).`);
-    match.leave(client);
+  @SubscribeMessage('pong.match_making.entry')
+  async receiveMatchMakingEntry(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: PongMatchMakingEntryDTO
+  ) {
+    const user = await this.authService.trapAuth(client);
+    if (!user) {
+      return;
+    }
+    // 待機キューにユーザーを追加する
+    const queue = this.waitingQueues.getQueue(data.queueID);
+    queue?.append(user.id);
   }
 
   @SubscribeMessage('pong.match.action')
-  receivePlayerAction(
+  async receivePlayerAction(
     @ConnectedSocket() client: Socket,
     @MessageBody() playerAction: PongMatchActionDTO
   ) {
-    match.moveBar(client.id, playerAction);
+    const user = await this.authService.trapAuth(client);
+    if (!user) {
+      return;
+    }
+    this.ongoingMatches.moveBar(user.id, playerAction);
   }
 }
