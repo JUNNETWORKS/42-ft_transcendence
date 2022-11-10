80a760f48e26a310c729c051aa2c2214d781f5dc
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-11-10 13:32:09 +0900

    feat: 対象のユーザーとのdmRoomが存在するか確認

--- frontend/src/features/DM/DmModal.tsx
+++ frontend/src/features/DM/DmModal.tsx
@@ -2,6 +2,7 @@ import * as TD from '@/typedef';
 import { FTButton, FTTextField } from '@/components/FTBasicComponents';
 import { chatSocketAtom } from '@/stores/auth';
 import { useAtom } from 'jotai';
+import { dataAtom } from '@/stores/structure';
 
 type DmModalProps = {
   user: TD.User;
@@ -10,18 +11,33 @@ type DmModalProps = {
 
 export const DmModal = ({ user, onClose }: DmModalProps) => {
   const [mySocket] = useAtom(chatSocketAtom);
+  const [dmRooms] = useAtom(dataAtom.dmRoomsAtom);
   if (!mySocket) {
     onClose();
     return null;
   }
 
+  const dmRoomWithUser = () => {
+    if (!dmRooms) return undefined;
+    return dmRooms.find((room) => {
+      // TODO: dmRoomの方をDmRoomに変更
+      const dmRoom = room as TD.DmRoom;
+      if (dmRoom.roomMember.find((member) => member.userId === user.id)) {
+        return room;
+      }
+    });
+  };
+
   // 新規DM --> ルーム作成、メッセージ送信
   // DMがすでに存在 -> メッセージ送信
   const submit = () => {
     // DMのルームの存在確認
-    // ない場合、ルームを作成
+    const dmRoom = dmRoomWithUser();
+    if (!dmRoom) {
+      // ない場合、ルームを作成
+    }
     // DMの送信
-    // DMのルームに移動、フォーカス
+    // TODO: DMのルームに移動、フォーカス
     onClose();
   };
 
