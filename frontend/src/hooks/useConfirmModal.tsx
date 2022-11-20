6083c774bcca3b1973bc1232034873fb567801c2
河田鷹介 <benefium@gmail.com>
2022-11-20 14:34:46 +0900

    Promiseのresolve/reject の保持をちょっとatomっぽくする(本質的にはatomではない)

--- frontend/src/hooks/useConfirmModal.tsx
+++ frontend/src/hooks/useConfirmModal.tsx
@@ -8,6 +8,15 @@ type ConfirmText = {
   denialLabel?: string;
 };
 
+type Resolve = (value: boolean | PromiseLike<boolean>) => void;
+type Reject = (reason?: any) => void;
+type Continuation = {
+  resolve: Resolve;
+  reject: Reject;
+};
+let confirmContinuation: Continuation | null = null;
+
+const ticker = atom(0); // 変更検知用, 値はどうでも良い
 const confirmModalAtom = {
   text: atom<ConfirmText>({
     body: 'body',
@@ -15,46 +24,72 @@ const confirmModalAtom = {
     denialLabel: 'denialLabel',
   }),
   isOpen: atom(false),
+  continuation: atom(
+    // 関数をatom化することは, 今のJotaiでは不可能または非常に困難なようなので,
+    // 関数自体はatomではないただの変数に入れておく.
+    // それだけだとJotaiが変更を検知しない(いつまでたっても初期値を返し続ける)ので, 変更検知のためだけのatom(ticker)を定義しておく.
+    (get) => {
+      get(ticker); // 変更検知用, 値はどうでも良い
+      return confirmContinuation;
+    },
+    (get, set, newValue: Continuation | null) => {
+      confirmContinuation = newValue;
+      set(ticker, get(ticker) ^ 1);
+    }
+  ),
 };
 
-let confirmResolve: ((value: boolean | PromiseLike<boolean>) => void) | null =
-  null;
-let confirmReject: ((reason?: any) => void) | null = null;
-
+/**
+ * 二択モーダルダイアログを表示するフック.
+ * このフックが返す関数を呼び出すと,
+ * 「2つのボタンのあるダイアログを表示し、いずれかのボタンが押されるとresolveされるPromise」
+ * を返す.
+ */
 export const useConfirmModal = () => {
   const [, setText] = useAtom(confirmModalAtom.text);
   const [, setIsOpen] = useAtom(confirmModalAtom.isOpen);
-  return (body: string, arg?: Omit<ConfirmText, 'body'>) => {
+  const [, setContinuation] = useAtom(confirmModalAtom.continuation);
+  /**
+   * 二択モーダルダイアログを表示する
+   * @param body 本文
+   * @param arg オプション(ボタンラベルなど)
+   */
+  const confirm = (body: string, arg: Omit<ConfirmText, 'body'> = {}) => {
     return new Promise<boolean>((resolve, reject) => {
       setText({
-        ...(arg || {}),
+        ...arg,
         body,
       });
-      confirmResolve = resolve;
-      confirmReject = reject;
+      setContinuation({ resolve, reject });
       setIsOpen(true);
     });
   };
+  return confirm;
 };
 
+/**
+ * 二択モーダルダイアログのコンポーネント.
+ * 常にDOMツリー上にあるようにすること.
+ */
 export const useConfirmModalComponent = () => {
   const [text] = useAtom(confirmModalAtom.text);
   const [isOpen, setIsOpen] = useAtom(confirmModalAtom.isOpen);
+  const [continuation, setContinuation] = useAtom(
+    confirmModalAtom.continuation
+  );
   const modal = () => {
     const affirmCloser = () => {
-      if (confirmResolve) {
-        confirmResolve(true);
+      if (continuation) {
+        continuation.resolve(true);
       }
-      confirmResolve = null;
-      confirmReject = null;
+      setContinuation(null);
       setIsOpen(false);
     };
     const denialCloser = () => {
-      if (confirmResolve) {
-        confirmResolve(false);
+      if (continuation) {
+        continuation.resolve(false);
       }
-      confirmResolve = null;
-      confirmReject = null;
+      setContinuation(null);
       setIsOpen(false);
     };
     return (
@@ -63,11 +98,11 @@ export const useConfirmModalComponent = () => {
           <div className="flex flex-row justify-center p-4">
             <p className="shrink-0 grow-0">{text.body}</p>
           </div>
-          <div className="flex flex-row justify-around">
-            <FTButton onClick={affirmCloser}>
+          <div className="flex flex-row justify-center">
+            <FTButton className="mx-2 min-w-[4em]" onClick={affirmCloser}>
               {text.affirmLabel || 'Yes'}
             </FTButton>
-            <FTButton onClick={denialCloser}>
+            <FTButton className="mx-2 min-w-[4em]" onClick={denialCloser}>
               {text.denialLabel || 'No'}
             </FTButton>
           </div>
