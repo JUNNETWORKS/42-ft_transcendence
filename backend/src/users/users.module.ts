f125cbab0dc6a443501bbf36c8706cd7cd35c018
河田鷹介 <benefium@gmail.com>
2022-11-22 01:23:49 +0900

    さらにマージ対応

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,6 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
 import { ChatModule } from 'src/chat/chat.module';
 import { ChatroomsModule } from 'src/chatrooms/chatrooms.module';
+import { WsServerModule } from 'src/ws-server/ws-server.module';
 
 import { PrismaModule } from '../prisma/prisma.module';
 import { MeController } from './me.controller';
@@ -19,6 +20,7 @@ import { UsersService } from './users.service';
     JwtModule,
     forwardRef(() => AuthModule),
     forwardRef(() => ChatModule),
+    WsServerModule,
   ],
   exports: [UsersService],
 })
