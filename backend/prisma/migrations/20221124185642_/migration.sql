bfe955bfa942616ce4533385e766530c284653e9
河田鷹介 <benefium@gmail.com>
2022-11-25 03:56:58 +0900

    migration

---
+++ backend/prisma/migrations/20221124185642_/migration.sql
@@ -0,0 +1,30 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "invalidateTokenIssuedBefore" TIMESTAMP(3),
+ADD COLUMN     "isEnabled2FA" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "isEnabledAvatar" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "UserAvatar" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "mime" TEXT NOT NULL,
+    "avatar" BYTEA NOT NULL,
+    "lastModified" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserAvatar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TotpSecret" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "secret" TEXT NOT NULL,
+
+    CONSTRAINT "TotpSecret_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserAvatar_userId_key" ON "UserAvatar"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TotpSecret_userId_key" ON "TotpSecret"("userId");
