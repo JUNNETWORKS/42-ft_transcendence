856f19f3c50b0ffc090496a889db385d4c51926d
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-01 23:44:37 +0900

    rest用のpostMessageを削除

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
-import { User, ChatRoom, ChatUserRelation } from '@prisma/client';
+import { ChatRoom, ChatUserRelation } from '@prisma/client';
 
 import * as Utils from 'src/utils';
 
-import { PostMessageDto } from '../chatrooms/dto/post-message.dto';
 import { OperationSayDto } from 'src/chatrooms/dto/operation-say.dto';
 
 import { ChatroomsService } from '../chatrooms/chatrooms.service';
@@ -16,10 +15,6 @@ export class ChatService {
     private readonly chatRoomService: ChatroomsService,
     private readonly userService: UsersService
   ) {}
-
-  async postMessage(data: PostMessageDto) {
-    return this.chatRoomService.postMessage(data);
-  }
 
   async postMessageBySay(data: OperationSayDto) {
     return this.chatRoomService.postMessage({
