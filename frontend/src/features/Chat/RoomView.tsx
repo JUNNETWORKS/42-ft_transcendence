e298b275d8b25f40fa0e5c2868a92f79d5c63b5d
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-03 01:12:39 +0900

    プライベートなルームでのみボタンを表示

--- frontend/src/features/Chat/RoomView.tsx
+++ frontend/src/features/Chat/RoomView.tsx
@@ -141,6 +141,8 @@ const MembersList = (props: {
   members: TD.UserRelationMap;
   memberOperations: TD.MemberOperations;
 }) => {
+  const isPrivate = props.room.roomType === 'PRIVATE';
+
   const computed = {
     members: useMemo(() => {
       const mems: TD.ChatUserRelation[] = [];
@@ -162,9 +164,11 @@ const MembersList = (props: {
   return (
     <div className="flex h-full flex-col">
       <FTH3 className="shrink-0 grow-0">Members</FTH3>
-      <div className="m-1">
-        <FTButton>invite</FTButton>
-      </div>
+      {isPrivate && (
+        <div className="m-1">
+          <FTButton>invite</FTButton>
+        </div>
+      )}
       <div className="shrink grow">
         {computed.members.map((member) => (
           <div key={member.userId}>
