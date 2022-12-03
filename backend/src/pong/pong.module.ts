96cee334038a55680f5b5f633018a7f3d88951a2
Junichi Sasaki <junichi200123@gmail.com>
2022-12-03 19:01:50 +0900

    MatchStrategyを作成し､OnlineMatch内でマッチが終了したタイミングで呼び出す

--- backend/src/pong/pong.module.ts
+++ backend/src/pong/pong.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PongService } from './pong.service';
-import { PongGateway } from './pong.gateway';
-import { PrismaModule } from '../prisma/prisma.module';
+
 import { AuthModule } from 'src/auth/auth.module';
 
+import { PrismaModule } from '../prisma/prisma.module';
+import { PostMatchStrategy } from './game/PostMatchStrategy';
+import { PongGateway } from './pong.gateway';
+import { PongService } from './pong.service';
+
 @Module({
-  providers: [PongService, PongGateway],
+  providers: [PongService, PongGateway, PostMatchStrategy],
   imports: [PrismaModule, AuthModule],
 })
 export class PongModule {}
