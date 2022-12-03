a94f5dc2c49680bf422884c3b6f06dae85156b27
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-03 17:29:08 +0900

    フロントTODO追加

--- frontend/src/features/Chat/components/InvitePrivateCard.tsx
+++ frontend/src/features/Chat/components/InvitePrivateCard.tsx
@@ -3,6 +3,7 @@ import { FTButton } from '@/components/FTBasicComponents';
 export const InvitePrivateCard = () => {
   // TODO: リストアップするユーザーを取得する
   // TODO: ユーザーをListboxとかで一覧表示する（このとき自分を表示しない）
+  // TODO: ページング実装（[<-], [->]みたいなボタンで）
   // TODO: 実行者が選択したユーザーをft_inviteで投げる
   // TODO: ft_inviteのレスポンスを表示する
 
