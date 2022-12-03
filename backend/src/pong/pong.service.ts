3a4b2c68548fcbe5ede66560b198af4529b7f61d
Junichi Sasaki <junichi200123@gmail.com>
2022-12-03 23:52:17 +0900

    GET /pong/ranking でランキング情報が取れるようになった｡上位10名

--- backend/src/pong/pong.service.ts
+++ backend/src/pong/pong.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
+import { pick } from 'src/utils';
+
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PongService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  async fetchUserRanking(count = 10) {
+    const results = await this.prisma.userRankPoint.findMany({
+      take: count,
+      orderBy: [
+        {
+          rankPoint: 'desc',
+        },
+        {
+          id: 'asc',
+        },
+      ],
+      include: {
+        user: {
+          select: {
+            id: true,
+            displayName: true,
+            isEnabledAvatar: true,
+          },
+        },
+      },
+    });
+    return results.map((result) => pick(result, 'rankPoint', 'user'));
+  }
 }
