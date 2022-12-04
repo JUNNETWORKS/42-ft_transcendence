3a155b46db953199039a0063616159e96a4cbfad
Taisei Toya <toyeah0102@gmail.com>
2022-12-04 19:39:22 +0900

    名前がはみ出ないよう修正

--- frontend/src/features/Pong/components/RankingCard.tsx
+++ frontend/src/features/Pong/components/RankingCard.tsx
@@ -15,7 +15,9 @@ export const RankingCard = ({
     <li className="flex items-center gap-5 bg-secondary py-2 px-3">
       <div className="w-16 text-4xl font-bold">{ordinal(rankPlace)}</div>
       <UserAvatar className="h-16 w-16" user={user} />
-      <div className="grow text-3xl">{user.displayName}</div>
+      <div className="max-w-[200px] grow truncate text-3xl">
+        {user.displayName}
+      </div>
       <div className="text-xl">{rankPoint}RP</div>
     </li>
   );
