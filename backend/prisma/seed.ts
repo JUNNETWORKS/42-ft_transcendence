abc5ff30a61938a2b9ed2ec91b2676c33a0af3b1
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-14 10:06:54 +0800

    seed追加

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -400,6 +400,44 @@ async function main() {
   //   });
   // }
   // await prisma.chatMessage.createMany({ data });
+
+  await prisma.chatMessage.create({
+    data: {
+      userId: 1,
+      chatRoomId: 1,
+      messageType: 'PR_OPEN',
+      content: 'PR_OPEN',
+    },
+  });
+
+  await prisma.chatMessage.create({
+    data: {
+      userId: 1,
+      chatRoomId: 1,
+      messageType: 'PR_CANCEL',
+      content: 'PR_CANCEL',
+    },
+  });
+
+  await prisma.chatMessage.create({
+    data: {
+      userId: 1,
+      secondaryUserId: 2,
+      chatRoomId: 1,
+      messageType: 'PR_START',
+      content: 'PR_START',
+    },
+  });
+
+  await prisma.chatMessage.create({
+    data: {
+      userId: 1,
+      secondaryUserId: 2,
+      chatRoomId: 1,
+      messageType: 'PR_RESULT',
+      content: 'PR_RESULT',
+    },
+  });
 }
 
 main()
