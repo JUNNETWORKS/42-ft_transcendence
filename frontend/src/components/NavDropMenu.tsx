3b23515bd405a51de20a05c2788b0771d18e60ef
河田鷹介 <benefium@gmail.com>
2022-12-26 17:13:28 +0900

    調整

--- frontend/src/components/NavDropMenu.tsx
+++ frontend/src/components/NavDropMenu.tsx
@@ -1,5 +1,6 @@
-import { Popover } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 import { useAtom } from 'jotai';
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 import { InlineIcon } from '@/hocs/InlineIcon';
@@ -19,29 +20,37 @@ export const NavDropMenu = () => {
       ]
     : [{ to: '/auth', cap: 'LogIn' }];
   return (
-    <Popover className="relative">
-      <Popover.Button className="outline-none">
-        <InlineIcon className="m-5 text-5xl" i={<MenuIcon.Hamburger />} />
-      </Popover.Button>
+    <Menu as="div" className="relative">
+      <Menu.Button className="m-4 rounded-lg p-1 outline-none hover:bg-secondary hover:text-black">
+        <InlineIcon className="text-5xl" i={<MenuIcon.Hamburger />} />
+      </Menu.Button>
 
-      <Popover.Panel className="absolute z-10">
-        <div className="flex w-28 flex-col bg-black text-3xl">
-          {links.map((link, i) => {
-            return (
-              <Popover.Button
-                key={i}
-                as={Link}
-                to={link.to}
-                className="border-2 border-solid border-white p-1"
-              >
-                {link.cap}
-              </Popover.Button>
-            );
-          })}
-        </div>
-
-        <img src="/solutions.jpg" alt="" />
-      </Popover.Panel>
-    </Popover>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute z-10 ml-2 origin-top-left">
+          <div className="flex w-36 flex-col bg-black text-3xl">
+            {links.map((link, i) => {
+              return (
+                <Menu.Item
+                  key={i}
+                  as={Link}
+                  to={link.to}
+                  className="border-2 border-b-0 border-solid border-white p-2 last:border-b-2 hover:bg-teal-700"
+                >
+                  {link.cap}
+                </Menu.Item>
+              );
+            })}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 };
