9dd53fe6199e3ab8323cabe9b5abffcf459ec4a8
河田鷹介 <benefium@gmail.com>
2022-12-27 02:19:07 +0900

    タイトル画面もスクロール

--- frontend/src/features/Index/Index.tsx
+++ frontend/src/features/Index/Index.tsx
@@ -2,18 +2,22 @@ import { Link } from 'react-router-dom';
 
 export const Index = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center gap-32 ">
-      <h1 className="text-8xl font-bold">ft_pong</h1>
-      <div className="flex flex-col gap-6">
-        <p className="text-5xl font-bold">
-          <Link to="/pong">Start</Link>
-        </p>
-        <p className="text-5xl font-bold">
-          <Link to="/me">Profile</Link>
-        </p>
-        <p className="text-5xl font-bold">
-          <Link to="/chat">Social</Link>
-        </p>
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className="flex max-h-[100%] w-full flex-col items-center overflow-y-scroll p-0">
+        <div className="flex flex-1 flex-col items-center justify-center gap-32">
+          <h1 className="text-8xl font-bold">ft_pong</h1>
+          <div className="flex flex-col gap-6">
+            <p className="text-5xl font-bold">
+              <Link to="/pong">Start</Link>
+            </p>
+            <p className="text-5xl font-bold">
+              <Link to="/me">Profile</Link>
+            </p>
+            <p className="text-5xl font-bold">
+              <Link to="/chat">Social</Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
