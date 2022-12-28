9e218c3b14890034fb206b5be417b956b9c0bc96
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-29 00:15:49 +0900

    fix

--- frontend/src/features/Index/Index.tsx
+++ frontend/src/features/Index/Index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const Index = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <div className="flex max-h-[100%] w-full flex-col items-center overflow-y-scroll p-0">
+      <div className="flex h-full max-h-[100%] w-full flex-col items-center overflow-y-auto p-0">
         <div className="flex flex-1 flex-col items-center justify-center gap-32">
           <h1 className="text-8xl font-bold">ft_pong</h1>
           <div className="flex flex-col gap-6">
