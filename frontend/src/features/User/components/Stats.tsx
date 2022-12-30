62b2e7ea0f5c28169a5931c58916adef76d7055d
河田鷹介 <benefium@gmail.com>
2022-12-30 18:10:28 +0900

    <UserStats>でpromiseを投げないようにする

--- frontend/src/features/User/components/Stats.tsx
+++ frontend/src/features/User/components/Stats.tsx
@@ -1,8 +1,6 @@
 import ordinal from 'ordinal';
-import { Suspense, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Suspense, useEffect, useState } from 'react';
 
-import { FTH3 } from '@/components/FTBasicComponents';
 import { useManualErrorBoundary } from '@/components/ManualErrorBoundary';
 import { APIError } from '@/errors/APIError';
 import { useAPICallerWithCredential } from '@/hooks/useAPICaller';
@@ -10,32 +8,34 @@ import { isfinite } from '@/utils';
 
 import { Stats } from '../types/MatchResult';
 
-const RenderStats = ({
-  id,
-  stats,
-  setter,
-  onError,
-}: {
+type Props = {
   id: number;
   stats: Stats | null;
   setter: (data: Stats) => void;
   onError: (e: unknown) => void;
-}) => {
+};
+
+const RenderStats = ({ id, stats, setter, onError }: Props) => {
   const callAPI = useAPICallerWithCredential();
-  if (!stats) {
-    throw (async () => {
-      try {
-        console.log(`users/${id}/pong/stats`);
-        const result = await callAPI('GET', `/users/${id}/pong/stats`);
-        if (!result.ok) {
-          throw new APIError(result.statusText, result);
+  useEffect(() => {
+    (async () => {
+      if (!stats) {
+        try {
+          console.log(`users/${id}/pong/stats`);
+          const result = await callAPI('GET', `/users/${id}/pong/stats`);
+          if (!result.ok) {
+            throw new APIError(result.statusText, result);
+          }
+          const json = await result.json();
+          setter(json);
+        } catch (e) {
+          onError(e);
         }
-        const json = await result.json();
-        setter(json);
-      } catch (e) {
-        onError(e);
       }
     })();
+  }, [callAPI, id, onError, setter, stats]);
+  if (!stats) {
+    return null;
   }
   return (
     <>
@@ -61,8 +61,8 @@ export const UserStats = ({ id }: { id: number }) => {
       <ErrorBoundary>
         <Suspense fallback={<p>Loading...</p>}>
           <RenderStats
-            id={id}
             stats={userStats}
+            id={id}
             setter={setter}
             onError={setError}
           />
